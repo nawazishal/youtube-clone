c13a9f3a818c19b16b3ff6876194654fc172f850
Nawazish Ali <50316441+nawazishal@users.noreply.github.com>
2019-09-02 15:29:02 +0500

    updated.

--- src/Components/VideoDetails.js
+++ src/Components/VideoDetails.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React from "react";
+import {Card,CardBody,CardText,CardSubtitle,Button} from 'reactstrap'
 
-import { Paper, Typography } from '@material-ui/core';
 
 const VideoDetails = ({ video }) => {
-  if(!video) return <div>Loading...</div>
+  if (!video) return <div>Loading...</div>;
 
-  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
 
   return (
     <React.Fragment>
-      <Paper  style={{ height: '15%' }}>
-        <iframe  height='100%' width='100%' title="Video Player" src={videoSrc}/>
-      </Paper>
-      <Paper  style={{ padding: '5px' }}>
-        <Typography variant="h4">{video.snippet.title} - {video.snippet.channelTitle}</Typography>
-        <Typography variant="subtitle1">{video.snippet.channelTitle}</Typography>
-        <Typography variant="subtitle2">{video.snippet.description}</Typography>
-      </Paper>
+      <Card>
+        
+        <CardBody>
+          <iframe src={videoSrc} alt='logo' height="500px" width="100%" name="iframe_a" title='video' />
+          <h4>{video.snippet.title} - {video.snippet.channelTitle}</h4>
+          <CardSubtitle> {video.snippet.channelTitle}</CardSubtitle>
+          <CardText>{video.snippet.description}</CardText>
+          <Button color='danger' class='btn btn-lg'>Subcribe</Button>
+          
+        </CardBody>
+        </Card>
+        
     </React.Fragment>
-  )
-}
+  );
+};
 
 export default VideoDetails;
